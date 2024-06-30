11ba0ef1ead71a3850f6d877201414f3fdc47f4e
Kim Harre <kim-harre@berlin.de>
2024-06-30 02:32:13 +0200

    asd

--- main.py
+++ main.py
@@ -8,11 +8,20 @@ import io
 # request each /api/object_detection
 # save each frame and add bbox
 
-def encode_image(image):
-    buffered = io.BytesIO()
-    image.save(buffered, format="JPEG")
-    encoded_string = base64.b64encode(buffered.getvalue()).decode("utf-8")
-    return encoded_string
+def frame_to_data_url(frame, image_format='png'):
+    # Encode the frame as an image
+    success, encoded_image = cv2.imencode(f'.{image_format}', frame)
+    
+    if not success:
+        raise ValueError("Could not encode the frame")
+    
+    # Convert the encoded image to a base64 string
+    base64_string = base64.b64encode(encoded_image).decode('utf-8')
+    
+    # Create the data URL
+    data_url = f'data:image/{image_format};base64,{base64_string}'
+    
+    return data_url
 
 def main():
     url = 'http://localhost:5000/api/object_detection'
@@ -39,24 +48,33 @@ def main():
     #create video writer
     out = cv2.VideoWriter('output.avi', codec, frame_rate, (frame_width, frame_height))
     #loop through frames
+
+
     for i in range(frame_count):
         #read frame
         ret, frame = cap.read()
-        #save frame
-        cv2.imwrite('frame.jpg', frame)
         #send request
-        encoded_string = encode_image(frame)
+        encoded_string = frame_to_data_url(frame)
+        print(encoded_string[:10])
         response = requests.post("http://127.0.0.1:5000/api/object_detection", json={"image": encoded_string})
         #get response
         data = response.json()
         #get bbox
-        bbox = data["detection"]['bbox']
+        print(data)
+        bbox = data["detection"]['bboxes']
         #draw bbox
-        cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 2)
+        for box in bbox:
+            x1, y1, x2, y2 = map(int, box)  # Convert coordinates to integers
+            print(x1, y1, x2, y2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         #save frame with bbox
         out.write(frame)
         #display frame
-        cv2.imshow('frame', frame)
         #wait for key
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+    #release video
+    cap.release()
+    out.release()
+
+main()
